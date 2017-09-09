f0cd298fd24ab638796a7838b22a2d101662ddfb
Kosuke Kusano <cocuh.kk@gmail.com>
2017-09-10 06:55:27 +0900

    integrate circle ci

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import find_packages, setup
 
+install_requires = [
+    'requests',
+    'numpy',
+    'tqdm',
+    'filelock',
+    'scipy',
+]
+tests_require = [
+    'nose',
+    'coverage',
+]
+
 setup(
   name='alea',
   version='0.0.1',
   packages=find_packages(exclude=['tests']),
   url='',
   license='Apache v2',
-  install_requires=[
-    'requests',
-    'numpy',
-    'tqdm',
-    'filelock',
-    'scipy',
-  ],
-  tests_require=[
-    'nose',
-    'coverage',
-  ],
+  install_requires=install_requires,
+  tests_require=tests_require,
+  extras_require={
+    'test': tests_require,
+  },
   author='cocuh',
   author_email='',
   description='',
